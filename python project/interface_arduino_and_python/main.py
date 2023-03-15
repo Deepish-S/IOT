0a4d2ac9fcf7b9271f21f6da23ad3d81adb87d40
Deepish S <79304831+Deepish-S@users.noreply.github.com>
2023-03-15 10:22:32 +0530

    Add files via upload

---
+++ python project/interface_arduino_and_python/main.py
@@ -0,0 +1,18 @@
+import time
+from pymata4 import pymata4
+
+triggerPin = 12
+echo_pin = 13
+board = pymata4.Pymata4()
+
+def the_callback(data):
+    print("distance : ", data[2])
+
+board.set_pin_mode_sonar(triggerPin,echo_pin, the_callback)
+
+while True:
+    try:
+        time.sleep(1)
+        board.sonar_read(triggerPin)
+    except Exception:
+        board.shutdown()
